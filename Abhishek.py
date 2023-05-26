b206298dcc2f63c8090a1ca516a6ede0331ce0a0
abhinfra11 <134672813+abhinfra11@users.noreply.github.com>
2023-05-26 11:21:37 +0530

    Update Abhishek.py

--- Abhishek.py
+++ Abhishek.py
@@ -1,1 +1,17 @@
-print('Hello')
+print('Name:-Abhishek Patil')
+print('Location:-Pune')
+print('Department:-Computer')
+
+
+
+
+x = 5
+y = 10
+
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
